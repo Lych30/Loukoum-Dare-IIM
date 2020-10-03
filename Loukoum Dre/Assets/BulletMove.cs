5a5933312447f91a6df2b5a50ed20f60bc3fc61a
Noé <noeguillaumin@gmail.com>
2020-10-03 18:00:25 +0200

    shoot

---
+++ Loukoum Dre/Assets/BulletMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour
+{
+    public float deathTime = 2.0f, bulletSpeed = 5.0f;
+    private Rigidbody2D rb;
+    public GameObject playerSr;
+    void Start()
+    {
+        Destroy(gameObject, deathTime);
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        if (GameObject.Find("Player").GetComponent<SpriteRenderer>().flipX) { bulletSpeed = -bulletSpeed ; }
+        else { bulletSpeed = math.abs(bulletSpeed); }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(bulletSpeed, transform.position.y);
+        print(playerSr.GetComponent<SpriteRenderer>().flipX);
+    }
+}
---
+++ Loukoum Dre/Assets/PlayerShoot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject playerBullet;
+    public Transform shootPoint;
+    private bool canShoot = true;
+    [Range(0, 2)]
+    public float shootDelay = 0.3f;
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            if (canShoot) {StartCoroutine(Shoot()); }
+        }
+    }
+
+    IEnumerator Shoot()
+    {
+        canShoot = false;
+        Instantiate(playerBullet, shootPoint.position, Quaternion.identity);
+        yield return new WaitForSeconds(shootDelay);
+        canShoot = true;
+    }
+}

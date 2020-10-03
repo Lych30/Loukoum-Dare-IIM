1cad3b475649df44122373f5f19c20031be1cba3
Leo <Aqu4men@gmail.com>
2020-10-03 12:17:11 +0200

    Code du bumper

--- Loukoum Dre/Assets/Script/Leopold/ExitGame.cs
+++ Loukoum Dre/Assets/Script/Leopold/ExitGame.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ExitGame : MonoBehaviour
 {
     public void ClickExit()
-
     {
         Application.Quit();
     }
+    public void LauchGame()
+    {
+        SceneManager.LoadScene("OtherSceneName", LoadSceneMode.Additive);
+    }
+   
 
 }
---
+++ Loukoum Dre/Assets/Script/Leopold/Pad.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pad : MonoBehaviour
+{
+   [SerializeField] private float xValue;
+   [SerializeField] private float yValue;
+    // Start is called before the first frame update
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "player")
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(xValue, yValue);
+        }
+    }
+}

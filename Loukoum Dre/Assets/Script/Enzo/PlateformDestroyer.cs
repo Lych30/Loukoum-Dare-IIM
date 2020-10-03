421554d2aafeffcc23e700da0281ab94192c594a
Bifflou <gut.enzo02122001@gmail.com>
2020-10-03 18:24:58 +0200

    platform pas finis
    hé merde

---
+++ Loukoum Dre/Assets/Script/Enzo/PlateformDestroyer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class PlateformDestroyer : MonoBehaviour
+{
+
+    private bool active = true;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && active)
+        {
+
+            StartCoroutine(PlateformDestroy());
+            StartCoroutine(PlateformActivate());
+
+        }
+    }
+
+    IEnumerator PlateformDestroy()
+    {
+        active = false;
+        yield return new WaitForSeconds(2);
+        gameObject.SetActive(false);
+    }
+
+    IEnumerator PlateformActivate()
+    {
+        yield return new WaitForSeconds(3);
+        print("cc");
+        gameObject.SetActive(true);
+        active = true;
+    }
+}

bcd672df91df22e5a671d9494499f766602f9ad6
Bifflou <gut.enzo02122001@gmail.com>
2020-10-03 19:28:29 +0200

    Plateforme Destroyer is okay
    c'est good

--- Loukoum Dre/Assets/Script/Enzo/PlateformDestroyer.cs
+++ Loukoum Dre/Assets/Script/Enzo/PlateformDestroyer.cs
@@ -3,34 +3,23 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
 
-public class PlateformDestroyer : MonoBehaviour
-{
-
+public class PlateformDestroyer : MonoBehaviour{
     private bool active = true;
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Player" && active)
-        {
-
+    private void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.tag == "Player" && active){
             StartCoroutine(PlateformDestroy());
-            StartCoroutine(PlateformActivate());
-
         }
     }
 
-    IEnumerator PlateformDestroy()
-    {
+    IEnumerator PlateformDestroy(){
         active = false;
         yield return new WaitForSeconds(2);
-        gameObject.SetActive(false);
-    }
-
-    IEnumerator PlateformActivate()
-    {
+        gameObject.GetComponent<Collider2D>().enabled = false;
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
         yield return new WaitForSeconds(3);
-        print("cc");
-        gameObject.SetActive(true);
+        gameObject.GetComponent<Collider2D>().enabled = true;
+        gameObject.GetComponent<SpriteRenderer>().enabled = true;
         active = true;
     }
 }
